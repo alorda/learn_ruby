0b35552999a612aa556678400afc204515a1b4aa
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-18 16:12:56 -0500

    calculator.rb Trying the multiple numbers multiply method

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -8,4 +8,12 @@ end
 
 def sum(array)
   array.inject(0) { |sum, number| sum + number }
-end 
+end
+
+def multiply(num1, num2)
+  num1 * num2
+end
+
+def multiply_several_numbers(*num)
+  num.inject(1) {|product, number| product * number }
+end
