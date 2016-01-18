659d4cf2da2d5e0eff11fd9422fb44692446fccd
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-18 16:43:39 -0500

    calculator.rb - Stretch methods WORK! mult sev nums and factorials.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -15,5 +15,11 @@ def multiply(num1, num2)
 end
 
 def multiply_several_numbers(*num)
-  num.inject(1) {|product, number| product * number }
+  num.inject(1) { |product, number| product * number }
+end
+
+def factorial(num) # Take a number
+  factorial_array = [1..num] # Knowing that number, you can make a range from 1 to that number
+  # Now run #inject on that array...
+  factorial_array.inject(1) { |product, number| product * number }
 end
