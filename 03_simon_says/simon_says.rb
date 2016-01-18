81a2436d4b3800dace960f857a051df0eb225187
Jonathan Alorda <jonathan.alorda@gmail.com>
2016-01-18 17:57:02 -0500

    simon_says.rb working on Titleize

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -9,7 +9,12 @@ end
 def repeat(word, num = 2)
   ([word] * num).join(" ")
 end
+# Remember what join can do for an array.
 
 def start_of_word(word, num)
   word[0, num]
 end
+
+def first_word(string)
+  string.split[0]
+end # Remember what split can do for you.
